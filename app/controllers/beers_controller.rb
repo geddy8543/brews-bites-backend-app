5d3ae56b2480dda7e6d69064002791bca03809d0
Gail Eddy <gaileddy68@gmail.com>
2021-11-24 14:10:10 -0600

    adds beer controller models and recipe controller models

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,2 +1,48 @@
 class BeersController < ApplicationController
+  def index
+    beers = Beer.all
+    render json: beers
+  end
+
+  def show
+    beer = Beer.find_by(id: params[:id])
+    render json: beer
+  end
+
+  def create
+    beer = Beer.new(
+      name: params["name"],
+      style: params["style"],
+      description: params["description"],
+    )
+    if beer.save
+      render json: beer
+    else
+      render json: { errors: beer.errors.full_messages },
+             status: 422
+    end
+  end
+
+  def update
+    beer_id = params["id"]
+    beer = Beer.find_by(id: beer_id)
+
+    beer.name = params["name"] || beer.name
+    beer.style = params["style"] || beer.style
+    beer.description = params["description"] || beer.description
+
+    if beer.save
+      render json: beer
+    else
+      render json: { errors: beer.errors.full_messages },
+             status: 422
+    end
+  end
+
+  def destroy
+    beer_id = params["id"]
+    beer = Beer.find_by(id: beer_id)
+    beer.destroy
+    render json: { message: "Beer successfully destroyed!" }
+  end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,55 @@
 class RecipesController < ApplicationController
+  def index
+    recipes = Recipe.all
+    render json: recipes
+  end
+
+  def show
+    recipe = Recipe.find_by(id: params[:id])
+    render json: recipe
+  end
+
+  def create
+    recipe = Recipe.new(
+      section_id: params["section_id"],
+      title: params["title"],
+      description: params["description"],
+      ingredients: params["ingredients"],
+      instructions: params["instructions"],
+      image_url: params["image_url"],
+    )
+    ### happy path
+    if recipe.save
+      render json: recipe
+      ### sad path
+    else
+      render json: { errors: recipe.errors.full_messages },
+             status: 422
+    end
+  end
+
+  def update
+    recipe_id = params["id"]
+    recipe = Recipe.find_by(id: recipe)
+
+    recipe.title = params["title"] || recipe.title
+    recipe.description = params["description"] || recipe.description
+    recipe.ingredients = params["ingredients"] || recipe.ingredients
+    recipe.instructions = params["instructions"] || recipe.instructions
+
+    if recipe.save
+      render json: recipe
+    else
+      render json: { errors: recipe.errors.full_message },
+             status: 422
+    end
+  end
+
+  def destroy
+    recipe_id = params["id"]
+    recipe = Recipe.find_by(id: recipe_id)
+
+    recipe.destroy
+    render json: { message: "Recipe succeessfully destroyed!" }
+  end
 end
