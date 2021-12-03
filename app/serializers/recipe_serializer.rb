49226d780640e693eb79784e50cc59376d543a5e
Gail Eddy <gaileddy68@gmail.com>
2021-12-02 18:55:34 -0600

    adds recipes serializer and attributes

---
+++ app/serializers/recipe_serializer.rb
@@ -0,0 +1,5 @@
+class RecipeSerializer < ActiveModel::Serializer
+  attributes :id, :section_id, :title, :description, :ingredients, :instructions, :image_url
+
+  has_many :pairings, has_many :beers, through: :pairings
+end
