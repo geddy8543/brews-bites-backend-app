699fa5741dcaceb9835ed880c82ea61257b2e878
Gail Eddy <gaileddy68@gmail.com>
2021-12-03 12:22:50 -0600

    adds the rest of the routes for backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   post "/users" => "users#create" ##sign up as a user
   post "/sessions" => "sessions#create" ##log in as a user
+
   get "/recipes" => "recipes#index" ##get all recipes
   get "/recipes/:id" => "recipes#show" ##get single recipe by id
   get "/users" => "users#index" ##get all users
+  get "/beers" => "beers#index"  ## get all beers
 
+
+  patch "/recipes" => "recipes#update" ## update existing recipe
+  delete "/recipes" => "recipes#destroy" ## delete a recipe
 end
