e9f9b7ef2aea9d51cd579cbba88bac2c720eff10
Gail Eddy <gaileddy68@gmail.com>
2021-12-02 18:18:19 -0600

    adds users and models to users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   post "/users" => "users#create" ##sign up as a user
   post "/sessions" => "sessions#create" ##log in as a user
-  get "/recipes" => "recipes#index"
-  get "/recipes/:id" => "recipes#show"
-  post "/recipes" => "recipes#create"
+  get "/recipes" => "recipes#index" ##get all recipes
+  get "/recipes/:id" => "recipes#show" ##get single recipe by id
+  get "/users" => "users#index" ##get all users
+
 end
