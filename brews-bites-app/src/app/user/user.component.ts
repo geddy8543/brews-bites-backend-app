96d2daf3bb7d76e979f7cb1f254c42a649a2065a
Gail Eddy <86028046+geddy8543@users.noreply.github.com>
2022-06-01 14:49:31 -0500

    adds route and logic to user component and user service

--- brews-bites-app/src/app/user/user.component.ts
+++ brews-bites-app/src/app/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  userResponse: BehaviorSubject<any> = new BehaviorSubject({});
+  users: BehaviorSubject<any> = new BehaviorSubject([]);
+  
+  constructor(private userService: UserService) { }
 
-  constructor() { }
 
   ngOnInit(): void {
+    this.onGetUsers();
+  }
+
+  onGetUsers() {
+    this.userService.getUsers().subscribe(
+      (response: any) => {
+        this.userResponse.next(response);
+      },
+      (error: any) => console.log(error),
+      () => {
+        this.users.next(this.userResponse.value)
+        console.log(this.userResponse.value)
+      }
+    )
   }
 
 }
--- brews-bites-app/src/app/user/user.service.ts
+++ brews-bites-app/src/app/user/user.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface User{
+  id:number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private apiUrl = 'http://localhost:3000/users';
 
-  constructor() { }
+  
+  constructor(private http:HttpClient) {}
+
+  getUsers(): Observable<User> {
+    return this.http.get<User>(this.apiUrl);
+  }
+
 }
