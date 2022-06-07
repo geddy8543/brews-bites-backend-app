7a2036422a0d39aeda572b7247dcd7edf29ce53f
Gail Eddy <86028046+geddy8543@users.noreply.github.com>
2022-06-07 15:19:39 -0500

    adds routes and fixes html logic

--- brews-bites-app/src/app/app-routing.module.ts
+++ brews-bites-app/src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import { WelcomeComponent } from './welcome/welcome.component';
 import { UserComponent } from './user/user.component';
 import { BeerComponent } from './beer/beer.component';
 import { RecipeComponent } from './recipe/recipe.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
   { path: 'user', component: UserComponent},
   { path: 'beer', component: BeerComponent},
   { path: 'recipe', component: RecipeComponent},
+  { path: 'welcome', component: WelcomeComponent}
 ];
 
 @NgModule({
--- brews-bites-app/src/app/welcome/welcome.component.ts
+++ brews-bites-app/src/app/welcome/welcome.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Beer, BeerService } from '../beer/beer.service';
+import { Recipe, RecipeService } from '../recipe/recipe.service';
+
 
 @Component({
   selector: 'app-welcome',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
+  beers: Beer[] = [];
+  recipes: Recipe[] = [];
 
-  constructor() { }
+  constructor(private beerService: BeerService, recipeService: RecipeService) { }
 
   ngOnInit(): void {
+    this.getBeers();
+    this.getRecipes();
+  }
+
+  getBeers(): void {
+    // this.beerService.getBeers()
+    // .subscribe(beers => this.beers = beers);
+    console.log("get beers")
+  }
+
+  getRecipes(): void {
+    // this.recipeService.getRecipes()
+    // .subscribe(recipes => this.recipes = recipes);
+    console.log("get recipes")
   }
 
 }
