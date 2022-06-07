b2a4c291f2cdddb712beebbc2bbe90c1071e5bf8
Gail Eddy <86028046+geddy8543@users.noreply.github.com>
2022-06-07 10:21:30 -0500

    adds routing to components in app routing module

--- brews-bites-app/src/app/app-routing.module.ts
+++ brews-bites-app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { BeerComponent } from './beer/beer.component';
+import { RecipeComponent } from './recipe/recipe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'user', component: UserComponent},
+  { path: 'beer', component: BeerComponent},
+  { path: 'recipe', component: RecipeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
