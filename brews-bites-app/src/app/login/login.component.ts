2563b61a4e2810c17125df435d9a88caf0dbb2c6
Gail Eddy <86028046+geddy8543@users.noreply.github.com>
2022-06-10 08:54:53 -0500

    adds login component ts code

--- brews-bites-app/src/app/login/login.component.ts
+++ brews-bites-app/src/app/login/login.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+interface UserParams {
+  password?: string,
+  email?: string,
+  passwordConfirmation?: string,
+}
 
 @Component({
   selector: 'app-login',
@@ -6,11 +14,26 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  
-  constructor() { }
 
+  constructor(private http: HttpClient, private router: Router) { }
+
+  userParams: UserParams = { email: "", password: "" };
+  errors = [];
 
   ngOnInit(): void {
-    
-    }
   }
+
+  submit() {
+    this.http.post<any>('/api/users', this.userParams).subscribe({
+      next: (response) => {
+        console.log(response.data);
+        this.router.navigateByUrl("/login");
+      },
+      error: (error) => {
+        this.errors = error.response.data.errors;
+      }
+    });
+      
+  };
+
+}
