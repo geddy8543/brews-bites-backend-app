26e1d831553d0d2f80e25e59b24dc72f89b422c3
Gail Eddy <86028046+geddy8543@users.noreply.github.com>
2022-06-14 08:27:32 -0500

    fixed welcome component ts imports

--- brews-bites-app/src/app/welcome/welcome.component.ts
+++ brews-bites-app/src/app/welcome/welcome.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BeerService } from '../beer/beer.service';
-import { Recipe, RecipeService } from '../recipe/recipe.service';
+import { RecipeService } from '../recipe/recipe.service';
 
 
 @Component({
@@ -10,7 +10,7 @@ import { Recipe, RecipeService } from '../recipe/recipe.service';
 })
 export class WelcomeComponent implements OnInit {
   // beers: Beer[] = [];
-  recipes: Recipe[] = [];
+  // recipes: Recipe[] = [];
 
   constructor(private beerService: BeerService, recipeService: RecipeService) { }
 
